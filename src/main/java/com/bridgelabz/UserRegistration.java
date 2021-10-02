ccb674f8e60af2aafb11f1de0be04c23ec979865
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-10-02 21:18:47 +0530

    Added: User need to enter a valid First Name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,8 +1,19 @@
 package com.bridgelabz;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+/**
+ * Purpose  - As a User need to enter a valid First Name
+ * @author  - Utkarsh Mishra
+ * @version - 16.0
+ * @since   - 2021-10-02
+ */
 
 public class UserRegistration {
-    public static void main(String[] args) {
-        System.out.println("Welcome To User Registration System Using J-Unit Test Cases");
+
+    public boolean firstName(String firstName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
     }
 }
-
