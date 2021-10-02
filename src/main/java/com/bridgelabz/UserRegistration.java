88acb57b2c83de20951892c5295b67212afc5458
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-10-02 21:54:47 +0530

    User need to enter a valid Email Address

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -3,7 +3,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Purpose  - As a User need to enter a valid Last Name
+ * Purpose  - As a User need to enter a valid Email Address
  * @author  - Utkarsh Mishra
  * @version - 16.0
  * @since   - 2021-10-02
@@ -27,6 +27,16 @@ public class UserRegistration {
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
+
+    /*
+    Created a method to validate E-mail
+     */
+    public boolean email(String email) {
+        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
 }
